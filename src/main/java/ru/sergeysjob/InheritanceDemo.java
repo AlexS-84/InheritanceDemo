ec204748f84e5bee07541c8aed140be9f753b0f8
AlexS-84 <sergei_alekseev@bk.ru>
2022-05-19 21:55:35 +0500

    +Normal

--- src/main/java/ru/sergeysjob/InheritanceDemo.java
+++ src/main/java/ru/sergeysjob/InheritanceDemo.java
@@ -5,18 +5,19 @@ class InheritanceDemo {
     public static void main(String[] args) {
 	// write your code here
         NormalMember mem1 = new NormalMember("Сергей", 1, 2010);
-        VIPMember mem2 = new VIPMember("Евгений", 2, 2011);
-
-        //mem1.calculateAnnualFee();
-        //mem2.calculateAnnualFee();
-
-        //mem1.displayMemInfo();
-        //mem2.displayMemInfo();
+        NormalMember mem2 = new NormalMember("Наташа",3,2005);
+        VIPMember vmem1 = new VIPMember("Евгений", 2, 2011);
 
         mem1.setDiscount();
         mem1.calculateAnnualFee();
         mem1.displayMemInfo();
-        //
+
+        mem2.calculateAnnualFee();
+        mem2.displayMemInfo();
+
+        vmem1.calculateAnnualFee();
+        vmem1.displayMemInfo();
+
     }
 
 }
